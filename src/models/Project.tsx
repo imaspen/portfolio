1c97ef3c896c9df59e229d51746f7be36eeb8d19
Aspen Thompson <zebington@gmail.com>
2022-03-10 14:28:44 +0000

    Some placeholder projects

--- src/models/Project.tsx
+++ src/models/Project.tsx
@@ -2,6 +2,8 @@ import { ImageProps } from "next/image";
 import AocImage from "../../public/img/aoc.png";
 import LethalDealImage from "../../public/img/lethal_deal.png";
 import LethalDealDescription from "./descriptions/LethalDealDescription";
+import PortfolioImage from "../../public/img/portfolio.png";
+import OverlordImage from "../../public/img/overlord.png";
 
 export default interface IProject {
   id: string;
@@ -14,6 +16,22 @@ export default interface IProject {
 
 export const Projects: IProject[] = [
   {
+    id: "lethal-deal",
+    name: "Lethal Deal",
+    headerImage: LethalDealImage,
+    shortDescription:
+      "Final year university game project, written in UE4 with C++.\n“Doom meets Slay the Spire”",
+    longDescription: LethalDealDescription,
+  },
+  {
+    id: "portfolio",
+    name: "Portfolio",
+    headerImage: PortfolioImage,
+    headerPosition: "top center",
+    shortDescription:
+      "The site you’re on right now! I wrote it myself in TypeScript using Next.JS (React Framework) and MUI (Component Library).",
+  },
+  {
     id: "aoc",
     name: "Advent of Code",
     headerImage: AocImage,
@@ -22,16 +40,16 @@ export const Projects: IProject[] = [
       "A yearly set of Christmas themed coding challenges, allowing to test my skills in a number of languages.",
   },
   {
-    id: "lethal-deal",
-    name: "Lethal Deal",
-    headerImage: LethalDealImage,
+    id: "overlord",
+    name: "Overlord",
+    headerImage: OverlordImage,
+    headerPosition: "top center",
     shortDescription:
-      "Final year university game project, written in UE4 with C++.\n“Doom meets Slay the Spire”",
-    longDescription: LethalDealDescription,
+      "The administrator portal for my current employer, Beacon. Written using a mix of JavaScript and TypeScript, Overlord is a modern React app with a quick development cycle.",
   },
 ];
 
-const FavoriteProjectIds = ["aoc", "lethal-deal"];
+const FavoriteProjectIds = ["lethal-deal", "portfolio", "aoc", "overlord"];
 
 export const FavoriteProjects: IProject[] = FavoriteProjectIds.reduce(
   (acc: IProject[], favId: string) => {
